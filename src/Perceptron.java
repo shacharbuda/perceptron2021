ca82be4d081e44691472898070cf932897f00d18
Shachar Buda <shacharbuda27@gmail.com>
2021-01-14 19:55:00 +0200

    wip

---
+++ src/Perceptron.java
@@ -0,0 +1,9 @@
+public class Perceptron {
+    private double[] weights = new double[]{ 0.3, 0.5};
+
+    public int feedForward(Point pntInput) {
+        // x and y of point is the "2" inputs of the perceptron
+    }
+
+    public int
+}
